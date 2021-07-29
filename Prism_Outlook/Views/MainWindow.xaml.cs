bff12f498c2b64290f18dfab095dc405fa32cf6b
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-29 15:42:19 +0700

    1

--- Prism_Outlook/Views/MainWindow.xaml.cs
+++ Prism_Outlook/Views/MainWindow.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace Prism_Outlook.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
